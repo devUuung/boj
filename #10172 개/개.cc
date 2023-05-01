aef17fcb20c7d5ac064ca18374789843d015ea38
dev_ung <87903147+devUuung@users.noreply.github.com>
2023-05-01 16:02:50 +0900

    [Bronze V 10172] Title: 개, Time: 0 ms, Memory: 2020 KB -NKLCBHub

---
+++ #10172 개/개.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    printf("|\\_/|\n"
+           "|q p|   /}\n"
+           "( 0 )\"\"\"\\\n"
+           "|\"^\"`    |\n"
+           "||_/=\\\\__|");
+    return 0;
+}

f72e2c44149c2244d57a54be663deab15f0f73a9
dev_ung <87903147+devUuung@users.noreply.github.com>
2023-05-05 22:08:09 +0900

    [Bronze V 10871] Title: X보다 작은 수, Time: 416 ms, Memory: 2020 KB -NKLCBHub

---
+++ #10871 X보다 작은 수/X보다 작은 수.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N, X; cin >> N >> X;
+    for (int i = 0; i < N; i++) {
+        int temp; cin >> temp;
+        if (temp < X) cout << temp << " ";
+    }
+
+    return 0;
+}

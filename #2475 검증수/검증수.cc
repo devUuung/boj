dd208a857d411d69e426b1d1f6f03d7c3b2da741
dev_ung <87903147+devUuung@users.noreply.github.com>
2023-05-10 11:02:23 +0900

    [Bronze V 2475] Title: 검증수, Time: 0 ms, Memory: 2020 KB -NKLCBHub

---
+++ #2475 검증수/검증수.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int sum = 0;
+
+    for (int i = 0; i < 5; i++) {
+        int a; cin >> a;
+        sum += a*a;
+    }
+    cout << sum % 10;
+    return 0;
+}

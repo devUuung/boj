575e7487a6bd04c1d2f4bc313912b8a0d946650e
dev_ung <87903147+devUuung@users.noreply.github.com>
2023-05-29 23:40:11 +0900

    [Gold II 2749] Title: 피보나치 수 3, Time: 0 ms, Memory: 2024 KB -NKLCBHub

---
+++ #2749 피보나치 수 3/피보나치 수 3.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef vector<vector<long long>> matrix;
+matrix operator * (matrix &a, matrix &b) {
+    int n = a.size();
+    matrix c(n, vector<long long>(n));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < n; k++) {
+                c[i][j] += a[i][k] * b[k][j];
+            }
+            c[i][j] %= 1000000;
+        }
+    }
+    return c;
+}
+
+int main() {
+    cin.tie(0); cout.tie(0);
+    ios::sync_with_stdio(0);
+    unsigned long long N; cin >> N;
+    matrix result = {{1, 0}, {0, 1}};
+    matrix a = {{1, 1}, {1, 0}};
+    while (N > 0) {
+        if (N&1) {
+            result = result * a;
+        }
+        a = a * a;
+        N /= 2;
+    }
+    cout << result[0][1];
+    return 0;
+}
